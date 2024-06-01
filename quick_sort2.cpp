17ef0608dab8df4e618f5a2411530f684dc6a497
Anca Stanoev <150381628+ancastanoev@users.noreply.github.com>
2024-06-01 22:50:31 +0300

    Update quick_sort2.cpp

--- quick_sort2.cpp
+++ quick_sort2.cpp
@@ -1,34 +1,38 @@
-#include <stdio.h>
+#include<stdio.h>
 void swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-void quickSort2(int data[], int start, int end) {
-    int i = start, j = end;
-    int pivot = data[(start + end) / 2];
-    while (i <= j) {
-        while (data[i] < pivot)
-            i++;
-        while (data[j] > pivot)
-            j--;
-        if (i <= j) {
-            swap(&data[i], &data[j]);
-            i++;
-            j--;
+
+// Tail-recursive QuickSort function
+void quickSort(int data[], int left, int right) {
+    while (left < right) {
+        int i = left, j = right;
+        int pivot = data[(left + right) / 2];
+        
+        // Partitioning
+        while (i <= j) {
+            while (data[i] < pivot)
+                i++;
+            while (data[j] > pivot)
+                j--;
+            if (i <= j) {
+                swap(&data[i], &data[j]);
+                i++;
+                j--;
+            }
+        }
+
+        // Recursively sort the smaller partition first
+        if (j - left < right - i) {
+            if (left < j)
+                quickSort(data, left, j);
+            left = i;
+        } else {
+            if (i < right)
+                quickSort(data, i, right);
+            right = j;
         }
     }
-    if (start < j)
-        quickSort2(data, start, j);
-    if (i < end)
-        quickSort2(data, i, end);
-}
-int main() {
-    quickSort2(data, 0, arraySize - 1);
-    printf("Sorted array: ");
-    for (int i = 0; i < arraySize; i++) {
-        printf("%d ", data[i]);
-    }
-    printf("\n");
-    return 0;
 }
